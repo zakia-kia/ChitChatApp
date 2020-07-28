4915833b8285315c1b04b44bf20f2599fbe63726
zakia hasna azzahra <zakiahasna.azzahra03@gmail.com>
2020-07-28 23:12:39 +0700

    commit ke-6

--- app/src/main/java/com/zakia/idn/chitchatapp/activity/MainActivity.kt
+++ app/src/main/java/com/zakia/idn/chitchatapp/activity/MainActivity.kt
@@ -2,10 +2,13 @@ package com.zakia.idn.chitchatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
     }
 }
--- app/src/main/java/com/zakia/idn/chitchatapp/fragment/CallFragment.kt
+++ app/src/main/java/com/zakia/idn/chitchatapp/fragment/CallFragment.kt
@@ -7,15 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.zakia.idn.chitchatapp.R
 
-class ChatFragment : Fragment() {
+class CallFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        return inflater.inflate(R.layout.fragment_call, container, false)
     }
-
-
 }
+
