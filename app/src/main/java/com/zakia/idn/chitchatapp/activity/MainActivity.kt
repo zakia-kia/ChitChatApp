7d15ae7d618c61fd85409508540cfb0e15024ccc
FUJITSU <deanumainah124@gmail.com>
2020-08-04 17:28:49 +0700

    yu

--- app/src/main/java/com/zakia/idn/chitchatapp/activity/MainActivity.kt
+++ app/src/main/java/com/zakia/idn/chitchatapp/activity/MainActivity.kt
@@ -3,16 +3,23 @@ package com.zakia.idn.chitchatapp.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import com.zakia.idn.chitchatapp.R
+import com.zakia.idn.chitchatapp.fragment.ChatFragment
+import com.zakia.idn.chitchatapp.fragment.SettingFragment
+import com.zakia.idn.chitchatapp.model.Users
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+
     }
 }
