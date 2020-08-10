34c3882c40431e6422a631764f4198665d3b8999
zakia hasna azzahra <zakiahasna.azzahra03@gmail.com>
2020-08-10 22:40:04 +0700

    2

--- app/src/main/java/com/zakia/idn/chitchatapp/adapter/ChatAdapter.kt
+++ app/src/main/java/com/zakia/idn/chitchatapp/adapter/ChatAdapter.kt
@@ -1,4 +1,28 @@
 package com.zakia.idn.chitchatapp.adapter
 
-class ChatAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zakia.idn.chitchatapp.model.Chat
+
+class ChatAdapter (mContext: Context, mChatList: List<Chat>, imageUrl: String) :
+    RecyclerView.Adapter<ChatAdapter.ViewHolder?>(){
+  
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ChatAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
