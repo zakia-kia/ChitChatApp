049c25859badeccf4d7acdd7d2d82f58fe733314
FUJITSU <deanumainah124@gmail.com>
2020-08-11 09:07:58 +0700

    sett

---
+++ app/src/main/java/com/zakia/idn/chitchatapp/activity/AboutAppActivity.kt
@@ -0,0 +1,12 @@
+package com.zakia.idn.chitchatapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zakia.idn.chitchatapp.R
+
+class AboutAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
